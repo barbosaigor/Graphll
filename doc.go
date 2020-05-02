83173af5ab16602d9af48db7ec230639dc7d8570
Igor <igor.barbosa1920@gmail.com>
2020-05-02 15:48:58 -0300

    package documentation

---
+++ doc.go
@@ -0,0 +1,5 @@
+// Package graphll contains a simple but fast weighted dependency graph data structure,
+// having O(1) complexity for read and write operations.
+//
+// Each graph node has a weight, dependencies and a name attached.
+package graphll
